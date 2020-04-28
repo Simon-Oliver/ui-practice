f2e01b6f91de9690b838cbe8b10bd751b9050221
Simon-Oliver <stauffer.simon@outlook.com>
2020-04-29 00:32:13 +0200

    exploring theming with css var

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -4,12 +4,18 @@ import React, { useState } from 'react';
 
 function Example() {
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  const [isDark, setDarkTheme] = useState(false);
+
+  const onBTNClick = () => {
+    setDarkTheme(!isDark);
+    console.log(document.documentElement);
+    document.documentElement.style.setProperty('--color-secondary', isDark ? 'blue' : 'red');
+  };
 
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button className={styles.button} onClick={() => setCount(count + 1)}>
+      <p>{`${isDark}`}</p>
+      <button className={styles.button} onClick={() => onBTNClick()}>
         Click me
       </button>
     </div>
