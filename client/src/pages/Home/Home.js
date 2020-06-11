76405b1f9f2972c8678e7a001a785afa7409cf81
Simon-Oliver <stauffer.simon@outlook.com>
2020-06-11 22:33:41 +0200

    adde simple server and client

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [data, setData] = useState({});
+  const [isLoaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (isLoaded === false) {
+      fetch('http://localhost:5000/test')
+        .then((res) => res.json())
+        .then((data) => {
+          console.log('----------', data);
+          setData(data);
+          setLoaded(true);
+          console.log('isloaded ------------>', isLoaded);
+        });
+    }
+  });
   return (
     <div>
       <h1>Home</h1>
+      <p>{data.data}</p>
     </div>
   );
 };
