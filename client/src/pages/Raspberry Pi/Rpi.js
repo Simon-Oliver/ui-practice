dabdbd33635749b614dd55e7508ea65634d8336f
Simon-Oliver <stauffer.simon@outlook.com>
2020-06-12 22:03:54 +0200

    playing with hooks

--- client/src/pages/Raspberry Pi/Rpi.js
+++ client/src/pages/Raspberry Pi/Rpi.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styles from './rpi.module.css';
 
 const Rpi = () => {
+  const [items, setItems] = useState({ count: 0 });
+  const [scrollDepth, setScroll] = useState(0);
+
+  const updateState = (num) => {
+    document.documentElement.scrollTo(0, 290) || document.body.scrollTo(0, 290);
+    setItems({ count: items.count + num });
+  };
+
+  const getScroll = () => {
+    const scroll = document.documentElement.scrollTop || document.body.scrollTop;
+    console.log(scroll);
+    setScroll(scroll);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', getScroll);
+  });
+
   return (
     <div>
       <h1>Raspberry Pi</h1>
+      <p>{items.count}</p>
+      <p>{scrollDepth}</p>
+      <button onClick={() => updateState(5)}>Click Me</button>
+      <div className={styles.bottom}></div>
     </div>
   );
 };
